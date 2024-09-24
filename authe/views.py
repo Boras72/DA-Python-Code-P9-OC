3d33de60c01eab81b9b37485f0e2feddac149a42
Boras72D <borasomiko@gmail.com>
2024-09-24 18:10:30 +0200

    Initial commit

---
+++ authe/views.py
@@ -0,0 +1,54 @@
+from django.views.generic import View
+from . import forms
+from django.conf import settings
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+
+
+class LoginPage(View):
+    form_class=forms.LoginForm
+    template_name='authentification/connexion.html'
+    # méthode qui envoie un formulaire vierge, il attend q l'user clique sur connexion (input)
+    def get(self, request):        #self=classe (attributs de la classe: view, from_class, temlate_class)
+        form=self.form_class          #instance=objet (self représente l'instance actuelle de cette classe)
+        message=''
+        context={'form':form,
+                 'message':message  
+        }
+        return render(                 #return permet de retourner le formulaire fait dans le html
+            request,
+            self.template_name,
+            context
+        )    
+        
+      
+            
+     #     en cliquant sur l'input (connexion), il appelle la méthode POST  
+    def post(self, request):  #dans cette méthode, les 2 var du formulaire sont remplis dans l'objet POST
+        form = self.form_class(request.POST)   #resquest.POST=objet dynamique qui a 2 éléments : "user" ,"pswd"
+        if form.is_valid(): # condition de la forme du formulaire
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password']
+            )
+            if user is not None:  # condition sur les éléments du login (user/pswd)
+                login(request, user)
+                return render(                 
+            request,self.template_name,context)    
+                #return redirect('login')   #feed=app 
+            else:
+                message='user non found'
+            
+
+        message = 'Identifiants invalides.'
+        context = {
+            'form': form,
+            'message': message
+        }
+        return render(                 
+            request,
+            self.template_name,
+            context
+        )    
+class SignupPage(View):      
+        
